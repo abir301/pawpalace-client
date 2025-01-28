3f83af3a2b82ea9ab34b44e4e7bff4f0beb3a87d
abir718 <abir.hasan2003s@gmail.com>
2025-01-28 11:15:22 +0600

    changes to home page added based on login

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,27 +1,54 @@
-import { NavLink } from "react-router-dom";
-
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { authContext } from "./Authprovider";
+import { Menu, MenuHandler, MenuList, MenuItem } from "@material-tailwind/react";
 const Header = () => {
 
     //[#0A303A]
     //[#F04336]  
+    const { user, logOut} = useContext(authContext);
 
     return (
-        <div className="flex items-center justify-around">
-            <div className="flex flex-col items-center">
-                <img className="w-16" src="/public/images/paw.png" alt="" />
-                <p className="text-xl font-medium text-[#F04336] ">PawPalace</p>
-            </div>
-            <div className="flex gap-10">
-                <NavLink className={({ isActive }) => `font-medium ${isActive ? 'text-[#F04336]' : 'text-gray-400'}`} to='/' >Home</NavLink>
-                <NavLink className={({ isActive }) => `font-medium ${isActive ? 'text-[#F04336]' : 'text-gray-400'}`} to='/petlisting'>Pet Listing</NavLink>
-                <NavLink className={({ isActive }) => `font-medium ${isActive ? 'text-[#F04336]' : 'text-gray-400'}`} to='/donationcampaign'>Donation Campaign</NavLink>
-            </div>
-            <div className="flex gap-3">
-                <NavLink to="/login"><button className="font-medium border-[2px] border-[#F04336] bg-[#F04336] text-white px-3 py-2 rounded-lg">Login</button></NavLink>
-                <NavLink to="/register"><button className="font-medium border-[2px] border-[#F04336] text-[#F04336] px-3 py-2 rounded-lg">Register</button></NavLink>
-            </div>
+        <div>
+            
+            <div className="flex items-center justify-around">
+                <div className="md:flex flex-col items-center hidden ">
+                    <img className="w-16" src="/public/images/paw.png" alt="" />
+                    <p className="text-xl font-medium text-[#F04336] ">PawPalace</p>
+                </div>
+                <div className="flex gap-10">
+                    <NavLink className={({ isActive }) => `font-medium ${isActive ? 'text-[#F04336]' : 'text-gray-400'}`} to='/' >Home</NavLink>
+                    <NavLink className={({ isActive }) => `font-medium ${isActive ? 'text-[#F04336]' : 'text-gray-400'}`} to='/petlisting'>Pet Listing</NavLink>
+                    <NavLink className={({ isActive }) => `font-medium ${isActive ? 'text-[#F04336]' : 'text-gray-400'}`} to='/donationcampaign'>Donation Campaign</NavLink>
+                </div>
+                <div className="flex gap-3">
+                    {user && user.email ?
+                        (<>
+                            <Menu animate={{ mount: { y: 0 }, unmount: { y: 25 }, }}
+                            >
+                                <MenuHandler>
+                                    <img className="w-10" src={user.photoURL} title={user.displayName} alt="User Icon" />
+                                </MenuHandler>
+                                <MenuList>
+                                    <MenuItem className="hover:bg-gray-200"><Link to='/register'>Dashboard</Link></MenuItem>
+                                    <MenuItem className="hover:bg-gray-200" onClick={logOut}>Log Out</MenuItem>
+                                </MenuList>
+                            </Menu>
 
+                        </>)
+                        : (
+                            <>
+                                <NavLink to="/login"><button className="font-medium border-[2px] border-[#F04336] bg-[#F04336] text-white px-3 py-2 rounded-lg">Login</button></NavLink>
+                                <NavLink to="/register"><button className="font-medium border-[2px] border-[#F04336] text-[#F04336] px-3 py-2 rounded-lg">Register</button></NavLink>
+                            </>
+                        )}
+
+                </div>
+
+            </div>
         </div>
+        
+
     );
 };
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,23 @@
-
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
 import { Toaster } from 'react-hot-toast';
+import { useContext } from 'react';
+import Loading from './Components/Loading';
+import { authContext } from './Authprovider';
 
 const Home = () => {
+    const { loader } = useContext(authContext);
+
+    if (loader) {
+        return <Loading />;
+    }
     return (
         <div>
-            <Toaster/>
-            <Header></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
+            <Toaster />
+            <Header />
+            <Outlet />
+            <Footer />
         </div>
     );
 };
